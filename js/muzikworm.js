a3d34d9848a92d83d6d7f141bf112ee2e49826cc
muzikworm <whatsthereinaname.man@gmail.com>
2016-01-02 04:49:59 +0530

    backup commit

--- js/muzikworm.js
+++ js/muzikworm.js
@@ -24,3 +24,16 @@ $(document).ready(function(){
         $("#myCarousel").carousel("next");
     });
 });
+
+$( document ).ready(function() {
+    $("[rel='tooltip']").tooltip();    
+ 
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    ); 
+});
